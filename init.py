0135049ea0d73ac709880b91feaaa6fe6e3ae276
BMJ123123 <atr13@naver.com>
2023-03-25 18:24:59 +0900

    마이너 업데이트

--- init.py
+++ init.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     for name, place, phone, age in datas:
         dbc.insert("user_info", _id_name="user_id", name=name,
                    age=age, place=place,  phone_number=phone)
-        user = dbc.select_one("user_info")
+    users = dbc.select("user_info")
+    for user in users:
         for p in range(3):
             dbc.insert("beacon", _id_name="device_id",
-                       capacity=6, user_id=user[0], point=0)
+                        capacity=6, user_id=user[0], point=0)
 
     # dbc.insert("sensor_data", device_id=device[0], max=10, min=0, avg=5, datetime = datetime.now())
     print(*dbc.select("user_info", order_by="user_id"), sep="\n")
